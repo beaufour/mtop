c9dd05bfb71fd43b79fc351ad9440cf51906c26c
Allan Beaufour <allan@beaufour.dk>
2011-03-06 17:40:14 -0500

    And spell the param correctly....

--- mtop.py
+++ mtop.py
@@ -34,7 +34,7 @@ def main():
     (options, _) = parser.parse_args()
 
     try:
-        connection = Connection(options.server, slave_okey=True)
+        connection = Connection(options.server, slave_okay=True)
     except AutoReconnect, ex:
         print 'Connection to %s failed: %s' % (options.server, str(ex))
         return -1
