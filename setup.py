5eb64d57e9288d625d45f2c0ce63c3466f7b817e
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2011-06-15 13:44:17 -0400

    Add setup.py with a console entry point.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name='mtop',
+    version='0.1',
+    url='https://github.com/beaufour/mtop',
+    author='Allan Beaufour',
+    description='mtop is a top like tool for MongoDB.',
+    zip_safe=False,
+    install_requires=[
+        'pymongo'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'mtop = mtop:main'
+        ],
+    }
+)
