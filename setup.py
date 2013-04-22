041df7c5720f099ca3618584daa1ef60efc850f5
Allan Beaufour <allan@beaufour.dk>
2013-04-22 17:11:03 -0400

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 setup(
     name='mtop',
-    version='0.1',
+    version='0.2',
     url='https://github.com/beaufour/mtop',
     author='Allan Beaufour',
     description='mtop is a top like tool for MongoDB.',
