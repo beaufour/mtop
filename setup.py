8be77688f0ca154b4232bc37dd720130a5331bf1
Allan Beaufour <allan@beaufour.dk>
2014-07-22 18:44:51 -0400

    add one simple test and run it

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     install_requires=[
         'pymongo'
     ],
+    test_suite = "tests.get_tests",
     entry_points = {
         'console_scripts': [
             'mtop = mtop.main:main'
